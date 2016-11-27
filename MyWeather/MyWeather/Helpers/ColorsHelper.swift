7f82928990f93718b17255bc68ca7217e44a8406
sjsoad <sjsoad666@yandex.ru>
2016-11-27 10:46:52 +0200

    no message

---
+++ MyWeather/MyWeather/Helpers/ColorsHelper.swift
@@ -0,0 +1,37 @@
+//
+//  ColorsHelper.swift
+//  MyWeather
+//
+//  Created by Sergey Kostyan on 27.11.16.
+//  Copyright © 2016 Sergey Kostyan. All rights reserved.
+//
+
+import UIKit
+
+class ColorsHelper: NSObject {
+
+    struct sunFloweColor {
+        let normal = UIColor("#FFCE54")
+        let dark   = UIColor("#F6BB42")
+    }
+    
+    struct grapefruitColor {
+        let normal = UIColor("#ED5565")
+        let dark   = UIColor("#DA4453")
+    }
+    
+    struct grassColor {
+        let normal = UIColor("#A0D468")
+        let dark   = UIColor("#8CC152")
+    }
+    
+    struct blueJeansColor {
+        let normal = UIColor("#5D9CEC")
+        let dark   = UIColor("#4A89DC")
+    }
+    
+    struct darkGrayColor {
+        let normal = UIColor("#656D78")
+        let dark   = UIColor("#434A54")
+    }
+}
